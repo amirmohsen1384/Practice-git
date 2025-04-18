6424d9a76fe4b0eb2d6188e1731dcc40d1405002
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-04-18 10:20:46 +0330

    Second Commit

--- Sample.cpp
+++ Sample.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void Print() {
+    cout << "4033613066";
+}
+
 // This is a sample C++ program
 int main() {
-    cout << "Hello, world.";
+    cout << "Hello, world." << endl;
+    Print();
     return 0;
 }
