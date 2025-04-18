f5a879f11a26098ca6d08c3615704cce6416a287
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-04-18 10:25:51 +0330

    Revert "Second Commit"
    This reverts commit 6424d9a76fe4b0eb2d6188e1731dcc40d1405002.

--- Sample.cpp
+++ Sample.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void Print() {
-    cout << "4033613066";
-}
-
 // This is a sample C++ program
 int main() {
-    cout << "Hello, world." << endl;
-    Print();
+    cout << "Hello, world.";
     return 0;
 }
